e7f26dd0340d662ecacff2e24ed03951cd038caf
Scott Brenstuhl <brenstsr@miamioh.edu>
2016-12-30 00:52:37 -0800

    finished feature scaling mini-project

--- Project5/ud120-projects/k_means/k_means_cluster.py
+++ Project5/ud120-projects/k_means/k_means_cluster.py
@@ -48,9 +48,10 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-feature_3 = "total_payments"
+#feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
+#features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -59,7 +60,8 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, _ in finance_features:
+#for f1, f2, _ in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2)
 plt.show()
 
@@ -67,6 +69,12 @@ plt.show()
 ### for the data and store them to a list called pred
 
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+
+min_max_scaler = MinMaxScaler()
+scaled_data = min_max_scaler.fit_transform(data)
+
+min_max_scaler.transform(np.array([[1.,200000.,1000000.]]))
 
 pred = KMeans(n_clusters=2).fit_predict(data)
 
