4a1dd76c4b0dd29939416f67c6d97df3471eeb92
Scott Brenstuhl <brenstsr@miamioh.edu>
2016-12-30 15:47:37 -0800

    finished feature selection mini-project

--- Project5/ud120-projects/text_learning/vectorize_text.py
+++ Project5/ud120-projects/text_learning/vectorize_text.py
@@ -52,7 +52,7 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-            sigs = ["sara", "shackleton", "chris", "germani"]
+            sigs = ["sara", "shackleton", "chris", "germani", "sshacklensf"]
             for sig in sigs:
                 email_text = email_text.replace(sig, '')
                 
