f5adf30b0cc82b92a9f113255ad0f65a669eb5c9
Scott Brenstuhl <brenstsr@miamioh.edu>
2017-01-08 18:04:21 -0800

    adding not to 1:1 coach Ashutosh

--- Project5/ud120-projects/final_project/poi_id.py
+++ Project5/ud120-projects/final_project/poi_id.py
@@ -5,6 +5,8 @@ import pickle
 sys.path.append("../tools/")
 
 from sklearn.tree import DecisionTreeClassifier
+#from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+#from sklearn.grid_search import GridSearchCV
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
@@ -12,9 +14,14 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi', 'other', 'bonus', 'expenses', 'total_payments',
-                    'total_stock_value', 'to_messages']
-                    
+features_list = ['poi',
+                'deferred_income',
+                 'shared_receipt_with_poi',
+                 'deferral_payments',
+                 'to_messages',
+                 'from_messages',
+                 'from_this_person_to_poi']
+                
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
@@ -27,12 +34,8 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 ### Store to my_dataset for easy export below.
 from collections import defaultdict
-my_dataset = defaultdict(dict)
-
-for person in data_dict:
-    for field in data_dict[person]:
-        if field in features_list:
-            my_dataset[person][field] = data_dict[person][field]
+#my_dataset = defaultdict(dict)
+my_dataset = data_dict
 
 for person in my_dataset:
     for field in my_dataset[person]:
@@ -54,16 +57,7 @@ labels, features = targetFeatureSplit(data)
 # My exploration is in POI_writeup.ipynb using final model below
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=None,
-            max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
-            min_samples_split=1, min_weight_fraction_leaf=0.0,
-            presort=False, random_state=1809, splitter='best')
 
-# from sklearn.ensemble import AdaBoostClassifier
-# clf = AdaBoostClassifier(algorithm='SAMME.R', base_estimator=None,
-#           learning_rate=1.5, n_estimators=20, random_state=1809)
-            
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
@@ -72,9 +66,22 @@ clf = DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=N
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = train_test_split(
-    features, labels, test_size=0.3, random_state=1809)
+# from sklearn.cross_validation import train_test_split
+# features_train, features_test, labels_train, labels_test = train_test_split(
+#     features, labels, test_size=0.3, random_state=1809)
+
+
+clf = DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=4,
+            max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+            min_samples_split=4, min_weight_fraction_leaf=0.0,
+            presort=False, random_state=1809, splitter='best')
+
+# clf.fit(features_train, labels_train)
+# pred = clf.predict(features_test)
+# r = recall_score(labels_test,pred)
+# f = f1_score(labels_test,pred)   
+# p = precision_score(labels_test,pred)
+# print r,f,p
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
